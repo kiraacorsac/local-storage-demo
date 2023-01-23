c5195acc411ce7775afcd251db0a214a8147a838
KiraaCorsac <wire150@gmail.com>
2023-01-23 13:08:45 +0100

    local storage demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
+
+  // localStorage.setItem('test', 'Hello world!')
+  // localStorage.setItem('test', 1)
+  // localStorage.setItem('test2', 'hello')
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    console.log(localStorage.getItem(key))
+  }
+
+  const person = {
+    name: "Juraj",
+    favoriteAnimal: {
+      specie: "dog",
+      size: "big"
+    },
+    clothing: ["Shirt", "Pants", "Hat"],
+    numberOfHeads: 1
+  }
+
+  // console.log(JSON.stringify(person));
+  // console.log(JSON.parse(JSON.stringify(person)))
+
+  // sessionStorage.setItem("juraj", JSON.stringify(person))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div >
+      <input name="address" />
+
+      Hello!
+      <p>
+        {/* {JSON.parse(sessionStorage.getItem('juraj')).name} */}
+      </p>
     </div>
   );
 }
